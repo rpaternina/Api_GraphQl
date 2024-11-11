1b8fa1fae82363ebca4cd7dcd6571e9e5bee0465
rpaternina <robertpaternina65@gmail.com>
2024-11-11 17:40:53 -0500

    Creando una api con GraphQl

---
+++ src/main/java/com/robert/api_graphql/ApiGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.robert.api_graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGraphqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/robert/api_graphql/ApiGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robert.api_graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
